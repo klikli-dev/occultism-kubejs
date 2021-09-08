d104cdabf3a8881248e90c95a92d3edb34d3cf08
Kli Kli <klikli@outlook.at>
2021-09-08 12:20:58 +0200

    Add empty framework

---
+++ src/main/java/com/github/klikli_dev/occultism_kubejs/OccultismKubeJS.java
@@ -0,0 +1,41 @@
+/*
+ * MIT License
+ *
+ * Copyright 2020 klikli-dev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.github.klikli_dev.occultism_kubejs;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(OccultismKubeJS.MODID)
+public class OccultismKubeJS {
+
+    public static final String MODID = "occultism_kubejs";
+    public static final String NAME = "Occultism KubeJS";
+    public static final Logger LOGGER = LogManager.getLogger(MODID);
+    public static OccultismKubeJS INSTANCE;
+
+    public OccultismKubeJS() {
+        INSTANCE = this;
+
+    }
+}
