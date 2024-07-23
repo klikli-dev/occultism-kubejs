b06e60fd80208fab3302f2b0779356dd9bcb23a6
Kli Kli <klikli@outlook.at>
2024-07-23 18:42:11 +0200

    chore: enable all script examples

--- runs/client/kubejs/server_scripts/example.js
+++ runs/client/kubejs/server_scripts/example.js
@@ -8,16 +8,16 @@ ServerEvents.recipes((event) => {
     //Some examples of how to add recipes:
 
     // event.recipes.occultism.spirit_trade('minecraft:rotten_flesh', 'minecraft:bone') //Note that a trade recipe alone is not enough, it needs a corresponding spirit job that is set to perform this trade.
-    // event.recipes.occultism.spirit_fire('minecraft:emerald_ore', '#c:gems/emerald')
-    // event.recipes.occultism.crushing(
-    //     RecipeResult.of("#c:ores/iron", 2),
-    //     '#minecraft:swords'
-    // )
-    // event.recipes.occultism.miner(
-    //     //item, count, weight
-    //     WeightedRecipeResult.of('minecraft:wooden_pickaxe', 1, 100),
-    //     '#occultism:miners/master'
-    // )
+    event.recipes.occultism.spirit_fire('minecraft:emerald_ore', '#c:gems/emerald')
+    event.recipes.occultism.crushing(
+        RecipeResult.of("#c:ores/iron", 2),
+        '#minecraft:swords'
+    )
+    event.recipes.occultism.miner(
+        //item, count, weight
+        WeightedRecipeResult.of('minecraft:wooden_pickaxe', 1, 100),
+        '#occultism:miners/master'
+    )
     event.recipes.occultism.ritual(
         'occultism:spirit_lantern',
         [
