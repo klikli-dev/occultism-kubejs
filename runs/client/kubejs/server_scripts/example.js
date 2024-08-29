ad2b55c5297467213fb0c7de83130412bd212e22
Kli Kli <klikli@outlook.at>
2024-08-29 18:53:44 +0200

    chore: reproduce enigmatica issue https://github.com/EnigmaticaModpacks/Enigmatica10/issues/90

--- runs/client/kubejs/server_scripts/example.js
+++ runs/client/kubejs/server_scripts/example.js
@@ -29,3 +29,34 @@ ServerEvents.recipes((event) => {
         'occultism:craft_afrit'
     ).dummy("kubejs:dummy_ritual_thing").useItem('minecraft:egg')
 })
+
+ServerEvents.recipes((event) => {
+    const recipes = [];
+
+    event.forEachRecipe({ type: 'occultism:ritual' }, (r) => {
+        let recipe = JSON.parse(r.json);
+        let recipe_id = r.getId();
+        if(recipe_id.includes("warden"))
+            return;
+
+        if (recipe.spirit_max_age) {
+            if (recipe_id.includes('_trader')) {
+                // Remove decay from Traders
+                delete recipe.spirit_max_age;
+            } else {
+                // Reduce delay for all other spirits (weather/daytime rituals)
+                recipe.spirit_max_age = 10;
+            }
+        }
+
+        // Override default craft time for all recipes
+        recipe.duration = 20;
+        recipe.id = recipe_id;
+        recipes.push(recipe);
+    });
+
+    recipes.forEach((recipe) => {
+        recipe.type = 'occultism:ritual';
+        event.custom(recipe).id(recipe.id);
+    });
+});
